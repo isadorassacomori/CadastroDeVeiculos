f158afc8408abe8dec5e5c2b1d431dbc8249e469
Emanuelli Prigol Ceresoli <marcelocerezoli4@gmail.com>
2026-09-20 11:36:25 -0300

    Adicionando as importações, declaração de classes e Scanner e lista

--- CadastroVeiculos.java
+++ CadastroVeiculos.java
@@ -2,3 +2,20 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
+public class CadastroVeiculos {
+
+    static Scanner scanner = new Scanner(System.in);
+    static List<Veiculo> veiculos = new ArrayList<>();
+
+    public static void main(String[] args) {
+
+        int opcao;
+
+        do {
+            System.out.println("\n======= Cadastro de Veículos OO =======");
+            System.out.println("1 - Cadastrar Veículo");
+            System.out.println("2 - Listar Veículos");
+            System.out.println("3 - Consultar Veículo");
+            System.out.println("0 - Sair");
+            System.out.print("Escolha uma opção: ");
