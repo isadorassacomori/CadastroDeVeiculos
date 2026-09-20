2aaa54621c2109e63031732e56349e93bd5f38e3
Maria Eduarda Radin <mariaeduarda0711@outlook.com>
2026-09-20 16:22:01 -0300

    Atualiza classe Veiculo

--- Veiculo.java
+++ Veiculo.java
@@ -5,19 +5,51 @@ public class Veiculo {
     String modelo;
     int ano;
     String placa;
-    
-}
 
-Veiculo() {
+    Veiculo() {
 
-    this("Marca desconhecida", "Modelo desconhecido",
-        LocalDate.now().getYear(), "");
+        this("Marca desconhecida", "Modelo desconhecido",
+                LocalDate.now().getYear(), "");
 
-}
+    }
 
-Veiculo(String marca, String modelo, int ano, String placa) {
-    this.marca = marca;
-    this.modelo = modelo;
-    this.ano = ano;
-    this.placa = placa;
+    Veiculo(String marca, String modelo, int ano, String placa) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+        this.placa = placa;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
 }
